6090dca49ca6f371fa7e4867c7683ebe090db746
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2022-06-02 18:17:51 -0400

    Delete SEL_DASH.sh

--- SEL_DASH.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/dash
-FILE_TRAFFIC="$1"
-rm t.txt -v
-rm TX_RX_Nodes.txt -v
-cat $FILE_TRAFFIC | egrep "connecting" | awk -F" " '{print $2}' > t.txt
-cat $FILE_TRAFFIC | egrep "connecting" | awk -F" " '{print $5}' >> t.txt
-sort -u t.txt > TX_RX_Nodes.txt
